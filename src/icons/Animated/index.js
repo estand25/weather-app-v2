50ec0909a0a718c19369295d70716e936d59ab0a
Standley Eugene <estand25@gmail.com>
2020-06-13 19:40:17 -0500

    Add weather icons static/animated

---
+++ src/icons/Animated/index.js
@@ -0,0 +1,57 @@
+import cloudy from './cloudy.svg';
+
+import cloudyDay1 from './cloudyDay1.svg';
+import cloudyDay2 from './cloudyDay2.svg';
+import cloudyDay3 from './cloudyDay3.svg';
+
+import cloudyNight1 from './cloudyNight1.svg';
+import cloudyNight2 from './cloudyNight2.svg';
+import cloudyNight3 from './cloudyNight3.svg';
+
+import night from './night.svg';
+
+import rainy1 from './rainy1.svg';
+import rainy2 from './rainy2.svg';
+import rainy3 from './rainy3.svg';
+import rainy4 from './rainy4.svg';
+import rainy5 from './rainy5.svg';
+import rainy6 from './rainy6.svg';
+import rainy7 from './rainy7.svg';
+
+import snowy1 from './snowy1.svg';
+import snowy2 from './snowy2.svg';
+import snowy3 from './snowy3.svg';
+import snowy4 from './snowy4.svg';
+import snowy5 from './snowy5.svg';
+import snowy6 from './snowy6.svg';
+
+import thunder from './thunder.svg';
+
+import sunny from './sunny.svg';
+
+export 
+{
+    cloudy,
+    cloudyDay1,
+    cloudyDay2,
+    cloudyDay3,
+    cloudyNight1,
+    cloudyNight2,
+    cloudyNight3,
+    sunny,
+    night,
+    rainy1,
+    rainy2,
+    rainy3,
+    rainy4,
+    rainy5,
+    rainy6,
+    rainy7,
+    snowy1,
+    snowy2,
+    snowy3,
+    snowy4,
+    snowy5,
+    snowy6,
+    thunder,
+}
