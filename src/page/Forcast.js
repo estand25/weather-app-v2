4f547d9e21ddcbb989a8d8fefc34ccd644cf23d8
Standley Eugene <estand25@gmail.com>
2020-06-14 14:08:26 -0500

    Add created components to forcast page

--- src/page/Forcast.js
+++ src/page/Forcast.js
@@ -1,36 +1,84 @@
 import React, { useState, useEffect } from 'react'
-import axio from 'axios'
+import { FetchService, FormatService, GenerateCalendarService } from '../service'
+import { DayOfWeekForcast } from '../components'
 import { useSelector, useDispatch } from 'react-redux'
 
 
 const Forcast = () => {
     const [weekly, setWeekly] = useState([])
     const [isLoading, setLoading] = useState(false)
-    const [data, setData] = useState([])
+    const [zip, setZip] = useState('19475')
+
+    const asyncOpenData =  async() => {
+        return await 
+            FetchService.fetchJson(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&cnt=40&units=imperial&appid=fff46882afbf25c4f00498e88eb69975`,
+            'get')
+    } 
+
+    const populateIconList = () => {
+        asyncOpenData()
+            .then( i => {                                        
+                var weeklyData = FormatService
+                    .JsonDailyWeather(i);
+
+                var dayOfWeek = FormatService
+                    .JsonWeatherDayOfWeek(weeklyData);
+
+                var icons = FormatService
+                    .JsonWeatherIcon(weeklyData)
+
+                var generatedWeekly = GenerateCalendarService
+                    .GenerateWeeklyCards(
+                        dayOfWeek,
+                        weeklyData.map(i => i.main.temp_max),
+                        weeklyData.map(i => i.main.temp_min),
+                        icons
+                    );
+                
+                setWeekly(generatedWeekly)
+                setLoading(true)                  
+            })
+
+    }
 
     useEffect(
         () => {
-            async function asyncOpenData(){
-               var i = await JsonOpenData();
-               console.log(i);
-               return i;
-            }
-
-            console.log(asyncOpenData())
-            console.log(data)
+            populateIconList();
         },[]
     )
 
-    const JsonOpenData = () => {
-        return axio({
-            url: 'http://api.openweathermap.org/data/2.5/forecast?zip=19475,us&cnt=40&units=imperial&appid=fff46882afbf25c4f00498e88eb69975',
-            method: 'get'     
-        })
+    const onSubmitClick = async () =>{
+        populateIconList();
+    }
+
+    if(!isLoading){
+        return <p>Loading...</p>
     }
 
     return (
-        <div>
-            Test
+        <div className='form-group'>
+            <input
+                className='form-control'
+                style={{margin: 5}}
+                type='text'
+                value={zip}
+                onChange={un => setZip(un.target.value)}
+            />
+            <button
+                className='btn btn-success'
+                style={{margin: 5}}
+                onClick={onSubmitClick}
+            >
+                Submit
+            </button>
+            <div className='row'>
+                {weekly.map(w => (
+                    <DayOfWeekForcast
+                        key={w.id}
+                        {...w}
+                    />
+                ))}
+            </div>
         </div>
     )
 
