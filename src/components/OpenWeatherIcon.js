a190066130e1c840066e600afea2b6056e1e22fa
Standley Eugene <estand25@gmail.com>
2020-06-14 14:06:37 -0500

    Created app components

---
+++ src/components/OpenWeatherIcon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const OpenWeatherIcon = ({icon}) => {
+    const iLink  = `http://openweathermap.org/img/w/${icon}.png`;
+
+    return (
+        <div>
+            <img
+                src={iLink}
+                width='64'
+                height='64'
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default OpenWeatherIcon
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import DayOfWeek from './DayOfWeek'
+import DayOfWeekForcast from './DayOfWeekForcast'
+import OpenWeatherIcon from './OpenWeatherIcon'
+import WeatherIcon from './WeatherIcon'
+
+export {
+    DayOfWeek,
+    DayOfWeekForcast,
+    OpenWeatherIcon,
+    WeatherIcon
+}
