150d532de5c5fbc5267d86faf4ad22ed15a837c1
Standley Eugene <estand25@gmail.com>
2020-06-14 14:06:07 -0500

    Created general calendar items model

---
+++ src/model/CalendarItem.js
@@ -0,0 +1,11 @@
+class CalendarItem {
+    constructor(key, dayOfWeek, highTemp, lowTemp, weatherImage){
+        this.key = key;
+        this.dayOfWeek = dayOfWeek;
+        this.highTemp = highTemp;
+        this.lowTemp = lowTemp;
+        this.weatherImage = weatherImage;
+    }
+}
+
+export default CalendarItem;
